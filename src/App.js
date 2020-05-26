07b7befb843baf2275f1b440da6f5a9e713cae30
Vu Quoc Dat <datvuquoc@channelvn.net>
2020-05-26 20:53:20 +0700

    add html markup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import { Header } from "./components/layout";
 
 import AuthContext from "./contexts/auth";
+import { Dashboard } from "./containers/dashboard";
+
+
 
 function App() {
   const [authUser, setAuthUser] = useState(null);
   return (
-    <AuthContext.Provider value={{ authUser, setAuthUser }}>
-      <div className="App">
-        <Header />
-      </div>
+	  <AuthContext.Provider value={{ authUser, setAuthUser }}>
+		
+			<div className="App">
+				<Header />
+				<div className="flex-grow-1">
+					<Dashboard />
+				</div>
+			</div>
     </AuthContext.Provider>
+
   );
 }
 

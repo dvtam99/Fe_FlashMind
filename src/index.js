81dec82bc5cc1eb7abce49a8a597c9d6e45d85a1
Dao Van Tam <tamdv.neonstudio@gmail.com>
2020-05-30 10:43:45 +0700

    update css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
+import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>

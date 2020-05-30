5703747ef4e397a2cba3b3852c7e13a3d6f1f1cf
Dao Van Tam <tamdv.neonstudio@gmail.com>
2020-05-30 15:36:10 +0700

    update resgister

--- src/containers/auth/index.js
+++ src/containers/auth/index.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import Login from "./login";
 import Register from "./register";
 
-const Auth = (onHide) => {
+const Auth = (props) => {
   const [haveAccount, setHaveAccount] = useState(true);
   return (
     <div>
       {haveAccount ? (
-        <Login onHide={onHide} onMoveToRegister={() => setHaveAccount(false)} />
+        <Login
+          onHide={props.onHide}
+          onMoveToRegister={() => setHaveAccount(false)}
+        />
       ) : (
         <Register onMoveToLogin={() => setHaveAccount(true)} />
       )}
